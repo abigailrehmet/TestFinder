f6e7fa9b4720df347ccfa201b14cb3730050f4f3
isaacfrumkin <ifrumki1@jhu.edu>
2020-09-12 14:15:28 -0400

    added site page java

---
+++ app/src/main/java/com/example/testfinder/SiteActivity.java
@@ -0,0 +1,13 @@
+package com.example.testfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_site);
+    }
+}
