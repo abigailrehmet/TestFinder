f9f62e61154d3ab04e8ab687285f0c03cdd88ae6
isaacfrumkin <ifrumki1@jhu.edu>
2020-09-12 15:07:44 -0400

    home page

--- app/src/main/java/com/example/testfinder/ZipCodeActivity.java
+++ app/src/main/java/com/example/testfinder/ZipCodeActivity.java
@@ -3,14 +3,32 @@ package com.example.testfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ZipCodeActivity extends AppCompatActivity {
+
+    Button   mButton;
+    EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zip_code);
 
+        mButton = findViewById(R.id.button);
+        mEdit   = findViewById(R.id.edittext);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", mEdit.getText().toString());
+                    }
+                });
 
 
 
