a362d6e8412769511e2f112f7dceb85d61efa395
Justin Canedy <jcanedy1@jhu.edu>
2020-09-12 14:05:24 -0400

    TEEHEE A SMOL FIX

--- app/src/main/java/com/example/testfinder/ZipCodeActivity.java
+++ app/src/main/java/com/example/testfinder/ZipCodeActivity.java
@@ -1,8 +1,14 @@
 package com.example.testfinder;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+public class ZipCodeActivity extends AppCompatActivity {
 
-public class ZipCodeActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zip_code);
+    }
 }
